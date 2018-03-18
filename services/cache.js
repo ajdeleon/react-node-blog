d666193ca980b94adba1652b4fb3651e759fa9f4
AJ de Leon <ajdeleon1064@gmail.com>
2018-03-18 14:16:19 -0400

    add cleanCache middleware to post

--- services/cache.js
+++ services/cache.js
@@ -1,9 +1,50 @@
 const mongoose = require('mongoose')
+const redis = require('redis')
+const util = require('util')
 
+const redisUrl = 'redis://127.0.0.1:6379'
+const client = redis.createClient(redisUrl)
+
+client.hget = util.promisify(client.hget)
 const exec = mongoose.Query.prototype.exec
 
-mongoose.Query.prototype.exec = function() {
-  console.log('About to run query')
-  
-  return exec.apply(this, arguments)
+mongoose.Query.prototype.cache = function(options = {}) {
+  this.useCache = true
+  this.hashKey = JSON.stringify(options.key || '')
+
+  return this
+}
+
+mongoose.Query.prototype.exec = async function() {  
+  if (!this.useCache) {
+    return exec.apply(this, arguments)
+  }
+
+  const key = JSON.stringify(Object.assign({}, this.getQuery(), {
+    collection: this.mongooseCollection.name
+  }))
+
+  // See if key exists in redi
+  const cacheValue = await client.hget(this.hashKey, key)
+
+  // If so return it
+  if (cacheValue) {
+    const doc = JSON.parse(cacheValue)
+
+    return Array.isArray(doc)
+    ? doc.map(d => new this.model(d))
+    : new this.model(doc)
+  }
+  // If not, issue query and store new key in redis
+  const result = await exec.apply(this, arguments)
+
+  client.hset(this.hashKey, key, JSON.stringify(result), 'EX', 10)
+
+  return result
+}
+
+module.exports = {
+  clearHash(hashKey) {
+    client.del(JSON.stringify(hashKey))
+  }
 }
